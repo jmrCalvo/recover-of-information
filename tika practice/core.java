5ff27c0c9a2094e408b088b59b4c2b6e1b8a0a0a
jmrCalvo <jmr_calvo@outlook.com>
2018-09-24 13:40:38 +0200

    the running in the folder and the files

--- tika practice/core.java
+++ tika practice/core.java
@@ -6,29 +6,64 @@ import org.apache.tika.sax.BodyContentHandler;
 import java.net.*;
 import org.apache.tika.metadata.Metadata;
 
+import org.apache.tika.language.LanguageIdentifier;
+import org.apache.tika.language.detect.LanguageDetector;
+import org.apache.tika.langdetect.OptimaizeLangDetector;
+import org.apache.tika.language.detect.LanguageResult;
+
 public class core{
-  public static void main(String[] args) throws Exception{
-      Tika tika=new Tika();
-      for (String f : args){
-          File file=new File(f);
-          InputStream is=new FileInputStream(file);
-          Metadata metadata=new Metadata();
-          BodyContentHandler ch=new BodyContentHandler(-1);
-          ParseContext parseContext=new ParseContext();
-          AutoDetectParser parser=new AutoDetectParser();
-
-          parser.parse(is,ch,metadata,parseContext);
-
-          System.out.println("ch  "+ch.toString());
 
 
-          for(String name : metadata.names()){
-            String valor=metadata.get(name);
-            if(valor!=null){
-              System.out.println("metadata:"+name+" "+valor);
-            }
+
+  public static String identifyLanguage(String text) throws IOException{
+
+    LanguageDetector identifier=new OptimaizeLangDetector().loadModels();
+    LanguageResult idioma=identifier.detect(text);
+    System.out.print(idioma.getLanguage()+"\n");
+    return idioma.getLanguage();
+
+  }
+
+  public static void AllMetadata(String f)throws Exception{
+        Tika tika=new Tika();
+        File file=new File(f);
+        InputStream is=new FileInputStream(file);
+        Metadata metadata=new Metadata();
+        BodyContentHandler ch=new BodyContentHandler(-1);
+        ParseContext parseContext=new ParseContext();
+        AutoDetectParser parser=new AutoDetectParser();
+
+        parser.parse(is,ch,metadata,parseContext);
+        //System.out.println("ch  "+ch.toString());
+        identifyLanguage(ch.toString());
+
+        for(String name : metadata.names()){
+          String valor=metadata.get(name);
+          if(valor!=null){
+            //System.out.println("metadata: "+name+" "+valor);
           }
-          System.out.println("la  "+metadata.get(Metadata.CONTENT_TYPE));
-    }
- }
+        }
+        //System.out.println("la  "+metadata.get(Metadata.CONTENT_TYPE));
+
+  }
+
+  public static void WalkFile(String dir)throws Exception{
+      boolean result;
+      File folderFile = new File(dir);
+          if ((result = folderFile.exists())) {
+              File[] files = folderFile.listFiles();
+              for (File file : files) {
+                  boolean isFolder = file.isDirectory();
+                  //System.out.println((isFolder ? "FOLDER: " : "  FILE: ") + file.getName());
+                  if (isFolder){WalkFile(file.toString());}
+                  else{AllMetadata(file.toString());}
+              }
+          }
+  }
+
+
+  public static void main(String[] args) throws Exception{
+      WalkFile(args[0]);
+  }
+
 }
