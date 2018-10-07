f9925e4924c06c2d6086e6da967dc6f40d713784
jmrCalvo <jmr_calvo@outlook.com>
2018-10-07 21:12:05 +0200

    the first practice to implement a parser

---
+++ Lucene practice/core.java
@@ -0,0 +1,30 @@
+import java.io.File;
+import org.apache.tika.Tika;
+import org.apache.tika.parser.*;
+import java.io.*;
+import org.apache.tika.sax.BodyContentHandler;
+import java.net.*;
+import org.apache.tika.metadata.Metadata;
+
+import org.apache.tika.language.LanguageIdentifier;
+import org.apache.tika.language.detect.LanguageDetector;
+import org.apache.tika.langdetect.OptimaizeLangDetector;
+import org.apache.tika.language.detect.LanguageResult;
+import org.apache.tika.parser.txt.TXTParser;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+import java.util.*;
+import javax.swing.*;
+import java.lang.Math;
+
+
+
+public class core{
+
+
+
+  public static void main(String[] args) throws Exception{
+
+  }
+
+}
